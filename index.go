86c756c67ba8df7cad52d44b390bb12209f6bf4f
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2019-11-05 15:43:41 +0800

    slice colon index

---
+++ index.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Index("asfa_", "_"))
+	fmt.Println("asfa_"[1:])
+}
---
+++ slice_colon_index.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := "0123456"
+	fmt.Println(a[:])
+	fmt.Println(a[0:])
+	fmt.Println(a[6:])
+	fmt.Println(a[7:])
+	fmt.Println(a[8:])
+}
