2ca1faf48ad9254bc158794d7f10fc422fe964cc
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2021-11-17 11:48:07 +0800

    trim space trial

--- sql_parse.go
+++ sql_parse.go
@@ -11,7 +11,8 @@ func main() {
 
 	// sql := `CREATE OR REPLACE FUNCTION func_name(res_table_name text) RETURNS TABLE(id int, value text) AS $$ DECLARE tag text = '青少年';  BEGIN   EXECUTE 'CREATE TABLE ' || res_table_name || '(id serial, value text);'; EXECUTE 'INSERT INTO ' || res_table_name || '(value) VALUES('|| quote_literal(tag) ||');';  END  $$  LANGUAGE PLPGSQL   RETURNS NULL ON NULL INPUT;`
 	// sql := `CREATE OR REPLACE FUNCTION func_name(res_table_name text) RETURNS SETOF RECORD AS $$ DECLARE tag text = '青少年';  BEGIN   EXECUTE 'CREATE TABLE ' || res_table_name || '(id serial, value text);'; EXECUTE 'INSERT INTO ' || res_table_name || '(value) VALUES('|| quote_literal(tag) ||');';  END  $$  LANGUAGE PLPGSQL   RETURNS NULL ON NULL INPUT;`
-	sql := `CREATE OR REPLACE FUNCTION func_name(res_table_name text, b OUT text) RETURNS SETOF RECORD AS $$ DECLARE tag text = '青少年';  BEGIN   EXECUTE 'CREATE TABLE ' || res_table_name || '(id serial, value text);'; EXECUTE 'INSERT INTO ' || res_table_name || '(value) VALUES('|| quote_literal(tag) ||');';  END  $$  LANGUAGE PLPGSQL   RETURNS NULL ON NULL INPUT;`
+	// sql := `CREATE OR REPLACE FUNCTION func_name(res_table_name text, b OUT text) RETURNS SETOF RECORD AS $$ DECLARE tag text = '青少年';  BEGIN   EXECUTE 'CREATE TABLE ' || res_table_name || '(id serial, value text);'; EXECUTE 'INSERT INTO ' || res_table_name || '(value) VALUES('|| quote_literal(tag) ||');';  END  $$  LANGUAGE PLPGSQL   RETURNS NULL ON NULL INPUT;`
+	sql := `CREATE OR REPLACE FUNCTION func_name(res_table_name text, b IN OUT text) RETURNS SETOF RECORD AS $$ DECLARE tag text = '青少年';  BEGIN   EXECUTE 'CREATE TABLE ' || res_table_name || '(id serial, value text);'; EXECUTE 'INSERT INTO ' || res_table_name || '(value) VALUES('|| quote_literal(tag) ||');';  END  $$  LANGUAGE PLPGSQL   RETURNS NULL ON NULL INPUT;`
 	tree, err := pg_query.ParseToJSON(sql)
 	if err != nil {
 		panic(err)
